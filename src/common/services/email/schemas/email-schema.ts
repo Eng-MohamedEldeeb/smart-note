a2be670bae15fabcb49deed360c4e87ac9f6cab8
Mohamed El-Deeb <dev.mohamed20@gmail.com>
2025-07-28 11:22:27 +0300

    Update email-schema.ts

--- src/common/services/email/schemas/email-schema.ts
+++ src/common/services/email/schemas/email-schema.ts
@@ -55,22 +55,3 @@ export const forgotPassword = ({
   }
 }
 
-export const verifyDeletion = ({
-  to,
-  otpCode,
-}: IEmailSchemaArgs): IEmailServiceArgs => {
-  return {
-    from: `"Intelli Note" <${process.env.EMAIL}>`,
-    to,
-    subject: 'Verify E-mail',
-    html: `
-          <div style="text-align: center;">
-              <h1>Hello</h1>
-              <h3>First we need you to confirm your e-mail by using the following code</h3>
-              <h2>${otpCode}</h2>
-          </div>
-    `,
-  }
-}
-
-export const verifyDeactivation = verifyDeletion
